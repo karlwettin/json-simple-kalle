9146597b960cad7e28e6c272f22adf8b99caee06
kalle <karl.wettin@gmail.com>
2011-12-05 08:22:44 +0100

    1.1-kalle-8 introduced ClassResolver in order to allow default class package etc in input json.

---
+++ src/main/java/org/json/simple/serialization/DefaultPackageClassResolver.java
@@ -0,0 +1,34 @@
+package org.json.simple.serialization;
+
+/**
+ * @author kalle
+ * @since 2011-12-05 08:19
+ */
+public class DefaultPackageClassResolver extends ClassResolver {
+
+  private String defaultPackage;
+
+  public DefaultPackageClassResolver() {
+  }
+
+  public DefaultPackageClassResolver(String defaultPackage) {
+    this.defaultPackage = defaultPackage;
+  }
+
+  @Override
+  public Class resolve(String name) throws ClassNotFoundException {
+    try {
+      return super.resolve(defaultPackage + "." + name);
+    } catch (ClassNotFoundException cnfe) {
+      return super.resolve(name);
+    }
+  }
+
+  public String getDefaultPackage() {
+    return defaultPackage;
+  }
+
+  public void setDefaultPackage(String defaultPackage) {
+    this.defaultPackage = defaultPackage;
+  }
+}
